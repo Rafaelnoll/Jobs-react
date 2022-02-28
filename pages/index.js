b166df720368df927d71ad9b845a67706ef37210
Rafael Noll <rafanoll2004@gmail.com>
2022-02-28 20:27:22 -0300

    addding Homepage body

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,30 @@
 import Navbar from "../src/components/Navbar";
+import Image from "next/image";
+import HomepageImage from "../styles/img/homepage-image.png";
 
 export default function Home() {
   return (
-    <section>
+    <>
       <Navbar />
-    </section>
+      <section className="homepage-body">
+        <div className="homepage-container">
+          <div className="homaepage-texts">
+            <h1 className="homepage-title">Encontre as melhores oportunidades de emprego</h1>
+            <h3 className="homepage-subtitle">Trabalhe de casa, remotamente, freelance,contrato, e full-time. Se inscreva na nossa newsletter semanal.</h3>
+          </div>
+          <div className="newsletter-form-container">
+            <form className="newsletter-form">
+              <input type="email" className="form-input" placeholder="Seu Endereço de Email" />
+              <button type="submit" className="form-button">Inscrever-se!</button>
+            </form>
+          </div>
+        </div>
+        <Image
+          src={HomepageImage}
+          width={1100}
+          height={1000}
+        />
+      </section>
+    </>
   )
 }
